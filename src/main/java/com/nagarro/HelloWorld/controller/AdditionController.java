64cfe92e1a5f5ce0fcd833d2778f84b3bd00ecd5
DeveloperHulk <67422972+DeveloperHulk@users.noreply.github.com>
2021-04-07 00:43:41 +0530

    Update AdditionController.java

--- src/main/java/com/nagarro/HelloWorld/controller/AdditionController.java
+++ src/main/java/com/nagarro/HelloWorld/controller/AdditionController.java
@@ -29,4 +29,17 @@ public class AdditionController {
         mv.setViewName("add-result");
         return mv;
     }
+ @RequestMapping(value = "/subtract")
+    public ModelAndView getSubtractionResult(HttpServletRequest request , HttpServletResponse response) {
+        ModelAndView mv= new ModelAndView();
+        
+        int a = Integer.parseInt(request.getParameter("t3"));
+        int b =  Integer.parseInt(request.getParameter("t4"));
+        
+        int c= a-b;
+        
+        mv.addObject("results", c);
+        mv.setViewName("add-result");
+        return mv;
+    }
 }
